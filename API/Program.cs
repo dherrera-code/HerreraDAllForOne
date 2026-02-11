30ab5af39c38f9e36949579ba950e85372942dd8
dherrera-code <dherr929@gmail.com>
2026-02-11 13:02:36 -0800

    Add cors policy to local host and build!

--- API/Program.cs
+++ API/Program.cs
@@ -17,6 +17,15 @@ builder.Services.AddScoped<Magic8BallService>();
 builder.Services.AddScoped<RestaurantPickerService>();
 builder.Services.AddScoped<GuessItService>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Client", policy =>
+    {
+        policy.WithOrigins("http://localhost:5500","http://127.0.0.1:5500","http://localhost:5173/")
+        .AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,6 +36,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("Client");
 
 app.UseAuthorization();
 
