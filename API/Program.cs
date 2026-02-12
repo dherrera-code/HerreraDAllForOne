5c905e7716fb06f8bff5faff14909f3d7da0131f
dherrera-code <dherr929@gmail.com>
2026-02-12 15:32:00 -0800

    updating cors policy to allow my static web app to gain access to API

--- API/Program.cs
+++ API/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("Client", policy =>
     {
-        policy.WithOrigins("http://localhost:5500","http://127.0.0.1:5500","http://localhost:5173")
+        policy.WithOrigins("http://localhost:5500","http://127.0.0.1:5500","http://localhost:5173","https://white-island-0ac32031e.1.azurestaticapps.net/")
         .AllowAnyHeader().AllowAnyMethod();
     });
 });
@@ -38,8 +38,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseCors("Client");
-app.UseHttpsRedirection();
 
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
