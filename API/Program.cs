d8fb141965c3b5053fe9bb7c398f5848eab758cc
dherrera-code <dherr929@gmail.com>
2026-02-12 15:42:35 -0800

    fixing cors

--- API/Program.cs
+++ API/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("Client", policy =>
     {
-        policy.WithOrigins("http://localhost:5500","http://127.0.0.1:5500","http://localhost:5173","https://white-island-0ac32031e.1.azurestaticapps.net/","https://white-island-0ac32031e.1.azurestaticapps.net/index.html")
+        policy.WithOrigins("http://localhost:5500","http://127.0.0.1:5500","http://localhost:5173","https://white-island-0ac32031e.1.azurestaticapps.net")
         .AllowAnyHeader().AllowAnyMethod();
     });
 });
